acce07356694f102329535269b3dd450bd618cc4
vturman <logical@nxt.ru>
2017-05-05 02:03:52 +0300

    fix CreatureSay target bug

--- packets/game/server.js
+++ packets/game/server.js
@@ -14,7 +14,7 @@ serverGamePackets.CreatureSay = function (sock, type, text, targetName) {
 
     p.writeD(sock.client.char.ObjectId);
     p.writeD(type);
-    p.writeS(targetName ? targetName : sock.client.char.Name);
+    if (targetName) p.writeS(targetName ? targetName);
     p.writeS(sock.client.char.Name);
 
     return p;
